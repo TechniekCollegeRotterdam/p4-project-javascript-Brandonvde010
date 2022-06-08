e0cc99fd7a6673be1adf35b28f74396d660ab63b
Brandonvde010 <brandonvanderent010@gmail.com>
2022-06-08 12:43:13 +0200

    video 40:45

--- index.js
+++ index.js
@@ -21,12 +21,13 @@ class sprite {
 
     update() {
         this.draw()
-        this.velocity.y += gravity
+        
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
             this.velocity.y = 0 
-        }
+        } else this.velocity.y += gravity
     }
 }
 
@@ -54,9 +55,18 @@ velocity: {
 }
 })
 
-
-
 console.log(player);
+
+const keys = {
+ a: {
+     pressed: false 
+ },
+ d: {
+    pressed: false 
+}
+}
+
+let  lastKey 
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -64,6 +74,40 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
+     
+    player.velocity.x = 0 
+
+    if (keys.a.pressed && lastKey === 'a') {
+        player.velocity.x = -1
+    } else if (keys.d.pressed && lastKey === 'd') {
+        player.velocity.x = 1
+    }
 }
 
 animate()
+
+window.addEventListener( 'keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+        break
+        case 'a':
+        keys.a.pressed = true 
+        lastKey = 'a'
+        break
+    }
+console.log(event.key);
+})
+
+window.addEventListener( 'keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.pressed = false
+        break
+        case 'a':
+        keys.a.pressed = false
+        break
+    }
+console.log(event.key);
+})
