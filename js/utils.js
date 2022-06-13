e8d8786762eb82aac5dc657e05ef290c6e99aa2d
Brandonvde010 <brandonvanderent010@gmail.com>
2022-06-13 12:53:06 +0200

    Game

--- js/utils.js
+++ js/utils.js
@@ -1,32 +1,37 @@
-function rectangularCollision ({rectangle1, rectangle2}) {
-    return (rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x 
-        && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width && rectangle1.attackBox.position.y 
-        + rectangle1.attackBox.height >= rectangle2.position.y && rectangle1.attackBox.position.y <= rectangle2.position.y
-        + rectangle2.height)
-}
-
-function determineWinner({player, enemy, timerId}) {
-    clearTimeout (timerId)
-    document.querySelector('#displayText') .style.display = 'flex'
+function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (
+      rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
+        rectangle2.position.x &&
+      rectangle1.attackBox.position.x <=
+        rectangle2.position.x + rectangle2.width &&
+      rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
+        rectangle2.position.y &&
+      rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    )
+  }
+  
+  function determineWinner({ player, enemy, timerId }) {
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
     if (player.health === enemy.health) {
-        document.querySelector('#displayText') .innerHTML = 'Gelijkspel'
+      document.querySelector('#displayText').innerHTML = 'Gelijkspel'
     } else if (player.health > enemy.health) {
-        document.querySelector('#displayText') .innerHTML = 'Speler 1 heeft gewonnen'
+      document.querySelector('#displayText').innerHTML = 'Speler 1 wint'
     } else if (player.health < enemy.health) {
-        document.querySelector('#displayText') .innerHTML = 'Speler 2 heeft gewonnen'
+      document.querySelector('#displayText').innerHTML = 'Speler 2 wint'
     }
-}
-
-let timer = 50
-function decreaseTimer() {
-if (timer > 0 )
-setTimeout (decreaseTimer, 1000)
-    {timer--
-    document.querySelector('#timer') .innerHTML = timer
-}
-
-if (timer === 0) {
-    determineWinner({player, enemy, timerId})
-}
-
-}
+  }
+  
+  let timer = 60
+  let timerId
+  function decreaseTimer() {
+    if (timer > 0) {
+      timerId = setTimeout(decreaseTimer, 1000)
+      timer--
+      document.querySelector('#timer').innerHTML = timer
+    }
+  
+    if (timer === 0) {
+      determineWinner({ player, enemy, timerId })
+    }
+  }
